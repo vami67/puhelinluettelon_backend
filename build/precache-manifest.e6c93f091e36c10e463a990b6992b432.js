15c4cbdf0fb49103fc2d0e38e32a889160e478fb
Mika Vahlberg <mika.vahlberg@iki.fi>
2020-03-27 15:19:36 +0200

    3.11 puhelinluettelo full stack

---
+++ build/precache-manifest.e6c93f091e36c10e463a990b6992b432.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f39c36a18839dc3b2a67284a46b6747",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b1f3f804da18a9508924",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5b06c8f8b2f396f15980",
+    "url": "/static/js/2.6ff66d5a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6ff66d5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1f3f804da18a9508924",
+    "url": "/static/js/main.a16875f8.chunk.js"
+  },
+  {
+    "revision": "f326c947c2bb131ebc6d",
+    "url": "/static/js/runtime-main.7900c9ab.js"
+  }
+]);
--- index.js
+++ index.js
@@ -12,6 +12,7 @@ morgan.token('body', function(req, res) {
 
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+app.use(express.static('build'))
 
 let persons =
     [
