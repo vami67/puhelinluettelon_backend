6b04afa824e4933435a09271e1964c512a23c63e
Mika Vahlberg <mika.vahlberg@iki.fi>
2020-03-31 12:44:54 +0300

    3.14: puhelinluettelo ja tietokanta, step2

--- index.js
+++ index.js
@@ -15,30 +15,6 @@ app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 app.use(express.static('build'))
 
-let persons =
-    [
-        {
-            "name": "Arto Hellas",
-            "number": "040-123456",
-            "id": 1
-        },
-        {
-            "name": "Ada Lovelace",
-            "number": "39-44-5323523",
-            "id": 2
-        },
-        {
-            "name": "Dan Abramov",
-            "number": "12-43-234345",
-            "id": 3
-        },
-        {
-            "name": "Mary Poppendieck",
-            "number": "39-23-6423122",
-            "id": 4
-        }
-    ]
-
 app.get('/api/persons', (request, response) => {
     Person.find({}).then(persons => {
         response.json(persons)
@@ -50,18 +26,6 @@ app.get('/api/persons/:id', (request, response) => {
         response.json(person.toJSON())
     })
 })
-
-app.delete('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
-    persons = persons.filter(person => person.id !== id)
-
-    res.status(204).end()
-})
-
-app.get('/info', (req, res) => {
-    res.send(`<p>Phonebook has info for ${persons.length} people<br/>${new Date()}<p>`)
-})
-
 
 app.post('/api/persons', (request, response) => {
     const body = request.body
@@ -85,31 +49,6 @@ const generateId = (min, max) => {
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-
-app.post('/api/persons', (req, res) => {
-    const body = req.body
-
-    if (!body.name || !body.number) {
-        return res.status(400).json({
-            error: 'name or number missing'
-        })
-    }
-
-    if (persons.find(person => person.name.toLowerCase() === body.name.toLowerCase())) {
-        return res.status(400).json({
-            error: 'name must be unique'
-        })
-    }
-
-    const person = {
-        name: body.name,
-        number: body.number,
-        id: generateId(5, 100),
-    }
-    persons = persons.concat(person)
-    res.json(person)
-})
-
 
 
 const PORT = process.env.PORT
